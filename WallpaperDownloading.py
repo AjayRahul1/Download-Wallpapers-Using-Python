935156a1cf8c0715386e96c923a39ad0e3e28faf
AjayRahul1 <p.ajayrahul02@gmail.com>
2022-10-23 12:58:47 +0530

    Python Project File Added

---
+++ WallpaperDownloading.py
@@ -0,0 +1,33 @@
+import requests
+import os
+import random
+import string
+
+def wallpaper_search_function(search_query):
+    wallpaper_search_url = f"https://wallhaven.cc/api/v1/search?q={search_query}"
+    wp_resources = requests.get(wallpaper_search_url)
+    json_data_of_wallpapers = wp_resources.json()
+    download_links =[]
+    for wallpaper in json_data_of_wallpapers["data"]:
+        download_links.append(wallpaper["path"])
+    return download_links
+
+def generate_id():
+    return ''.join(random.choices(string.ascii_lowercase+string.digits, k=6))
+
+def download_wallpapers(url, folder_to_be_created_for_wallpapers):
+    print(f"Downloading... {url}")
+    res = requests.get(url)
+    wallpaper_name = generate_id()
+    ext = os.path.splitext(url)[1]
+    download_link_path = f"{folder_to_be_created_for_wallpapers}\{wallpaper_name}{ext}"
+    open(download_link_path,'wb').write(res.content)
+
+wallpaper_search_name_input = input("Enter the name to download it's wallpapers: ")
+wallpaper_dl_url = wallpaper_search_function(wallpaper_search_name_input)
+parent_path = "D:\\Projects\\WallpaperDownloadingusingPython\\WallpaperDownloadLinks\\"
+folder_to_be_created_for_wallpapers = os.path.join(parent_path,wallpaper_search_name_input)
+os.mkdir(folder_to_be_created_for_wallpapers)
+print(folder_to_be_created_for_wallpapers)
+for url in wallpaper_dl_url:
+    download_wallpapers(url,folder_to_be_created_for_wallpapers)
